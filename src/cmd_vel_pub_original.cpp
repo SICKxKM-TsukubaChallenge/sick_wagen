33dfba852b91c14a7078836f13a3816b6feb2140
Yuji Ogusu <yuji.ogusu@sick.jp>
2023-04-29 12:01:19 +0900

    Initial commit

---
+++ src/cmd_vel_pub_original.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+
+geometry_msgs::Twist cmd_vel;
+geometry_msgs::Twist cmd_vel_joy;
+geometry_msgs::Twist cmd_vel_whill;
+sensor_msgs::Joy joy;
+
+
+void joy_callback(const sensor_msgs::Joy& joy_msg)
+{
+	cmd_vel_joy.linear.x = joy_msg.axes[3]/1.5; //3
+	cmd_vel_joy.angular.z = joy_msg.axes[2]/1; //1.5
+
+	if(joy_msg.buttons[5] == 1){
+	    cmd_vel_joy.linear.x = joy_msg.axes[3]/0.75; //1.5
+	    cmd_vel_joy.angular.z = joy_msg.axes[2]/0.75; //1
+    }
+    if(joy_msg.buttons[4] == 1){
+	    cmd_vel_joy.linear.x = joy_msg.axes[3]/8;
+	    cmd_vel_joy.angular.z = joy_msg.axes[2]/4;
+    }
+}
+
+void joy_callback2(const sensor_msgs::Joy& joy_msg)
+{
+	cmd_vel_whill.linear.x = joy_msg.axes[1]/4;// /4
+	cmd_vel_whill.angular.z = joy_msg.axes[0]/2;// /2
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "gamepad_pub");
+	ros::NodeHandle nh;
+	ros::Publisher cmd_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+	ros::Subscriber gamepad_joy_sub = nh.subscribe("/joy", 10, joy_callback);
+	ros::Subscriber whill_joy_sub = nh.subscribe("/whill/states/joy", 10, joy_callback2);
+
+	ros::Rate loop_rate(10);//10
+	while (ros::ok())
+	{
+		if(cmd_vel_whill.linear.x == 0 && cmd_vel_whill.angular.z == 0)
+		{
+			cmd_vel = cmd_vel_joy;
+		}else{
+			cmd_vel = cmd_vel_whill;
+		}
+		cmd_pub.publish(cmd_vel);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
+}
